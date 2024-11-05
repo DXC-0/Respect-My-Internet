8648895c0104eabbed786fd2d942ce77774ac79e
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2024-11-05 17:00:14 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 ####### RESPECT-MY-INTERNET UPDATE SCRIPT ########
 
-rm -rf /home/$USER/.config/opensnitch/respect-my-internet.txt
-rm -rf /home/$USER/.config/opensnitch/respect-my-freedom.txt
-curl https://raw.githubusercontent.com/TheCyberArcher/Respect-My-Internet/refs/heads/main/blocklist/respect-my-internet.txt >> /home/$USER/.config/opensnitch/respect-my-internet.txt
-curl https://raw.githubusercontent.com/TheCyberArcher/Respect-My-Internet/refs/heads/main/regex/respect-my-freedom.txt >> /home/$USER/.config/opensnitch/respect-my-freedom.txt
+rm -rf /etc/opensnitchd/respect-my-internet.txt
+rm -rf /etc/opensnitchd/regex/respect-my-freedom.txt
+curl https://raw.githubusercontent.com/TheCyberArcher/Respect-My-Internet/refs/heads/main/blocklist/respect-my-internet.txt >> /etc/opensnitchd/respect-my-internet.txt
+curl https://raw.githubusercontent.com/TheCyberArcher/Respect-My-Internet/refs/heads/main/regex/respect-my-freedom.txt >> /etc/opensnitchd/regex/respect-my-freedom.txt
