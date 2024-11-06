e2dfacb8f472255f9bddbab926620b86c42b997b
If cli is a gift from god, using arch makes us prophets † <62696671+TheCyberArcher@users.noreply.github.com>
2024-11-06 10:35:40 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 ####### RESPECT-MY-INTERNET UPDATE SCRIPT ########
 
 rm -rf /etc/opensnitchd/respect-my-internet.txt
-rm -rf /etc/opensnitchd/regex/respect-my-freedom.txt
+rm -rf /etc/opensnitchd/ip/ip-protection.txt
 curl https://raw.githubusercontent.com/TheCyberArcher/Respect-My-Internet/refs/heads/main/blocklist/respect-my-internet.txt >> /etc/opensnitchd/respect-my-internet.txt
 curl https://raw.githubusercontent.com/TheCyberArcher/Respect-My-Internet/refs/heads/main/blocklist/ip/ip-protection.txt >> /etc/opensnitchd/ip/ip-protection.txt
