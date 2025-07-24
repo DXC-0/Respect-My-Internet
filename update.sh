b0fe8cd7a9156e8561deecf01f024de212600ca6
DXC-0 <62696671+DXC-0@users.noreply.github.com>
2025-07-24 23:41:53 +0000

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 ####### RESPECT-MY-INTERNET UPDATE SCRIPT ########
 
-rm -rf /etc/opensnitchd/respect-my-internet.txt
+rm -rf /etc/opensnitchd/blocklist/respect-my-internet.txt
 rm -rf /etc/opensnitchd/ip/ip-protection.txt
-curl https://raw.githubusercontent.com/TheCyberArcher/Respect-My-Internet/refs/heads/main/blocklist/respect-my-internet.txt >> /etc/opensnitchd/respect-my-internet.txt
+curl https://raw.githubusercontent.com/TheCyberArcher/Respect-My-Internet/refs/heads/main/blocklist/respect-my-internet.txt >> /etc/opensnitchd/blocklist/respect-my-internet.txt
 curl https://raw.githubusercontent.com/TheCyberArcher/Respect-My-Internet/refs/heads/main/blocklist/ip/ip-protection.txt >> /etc/opensnitchd/ip/ip-protection.txt
